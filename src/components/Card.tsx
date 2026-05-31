e9ad0ecf23847ac78b3e297fcd4e2f52eabf175d
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2026-05-31 18:30:31 +0700

    Try to add multiple components

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+function Card() {
+  return (
+    <div className="border-2 border-gray p-4 rounded-lg bg-green-900">
+      
+      <div className="">
+        Profile Image
+      </div>
+      
+      <div className="text-left">
+        <h2 className="">Horse Name</h2>
+        <h2 className="">Horse Gender</h2>
+        <h2 className="">Horse Age</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Card
